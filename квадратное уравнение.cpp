b51edd78d9681808d3059ec6291968e1a996660b
ZaputriaevaES <111437343+ZaputriaevaES@users.noreply.github.com>
2022-08-17 22:24:10 +0300

    третья версия квадратки
    с ускоренным вычислением корня(наверное) и новыми случаями

--- квадратное уравнение.cpp
+++ квадратное уравнение.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <stdio.h>
 #include <TXLib.h>
 #include <math.h>
 
+double sqrtmy(float);
 void SquareSolver(int, int, int);
 void lineynoe(int, int);
 void qwadratnoe(int, int, int);
@@ -11,22 +12,41 @@ int main(void)
     int a = 0, b = 0, c = 0;
     printf("Эта программа решает квадратные уравнения c целыми коэффицентами вида: a * x^2 + b * x + c\n");
     printf("Введите три коэффицента квадратного уравнения a, b, c или q для завершения программы:");
-    while(scanf("%d%d%d", &a, &b, &c) == 3)
+    while(scanf("%d%d%d", &a, &b, &c) == 3 )
     {
         SquareSolver(a, b, c);
     }
     return 0;
 }
 
+double sqrtmy(float x)
+{
+    float a = 1.0, b = x, c = (a + b) / 2;
+    while(fabs(c * c - x) > 0.001)
+    {
+        if ((c * c - x) * (a * a - x) < 0)
+            b = c;
+        else
+            a = c;
+        c = (a + b) / 2;
+    }
+    return c;
+}
+
 void SquareSolver(int a, int b, int c)
 {
-    if (a == 0)
+    if (a == 0 && b != 0)
     {
         lineynoe(b, c);
     }
-    else
+    else if (a != 0)
     {
         qwadratnoe(a, b, c);
+    }
+    else
+    {
+        printf("Вы ввели недопустимые значения, попробуйте еще раз\n");
+        printf("Введите три коэффицента квадратного уравнения a, b, c или q для завершения программы:");
     }
 }
 
@@ -36,7 +56,7 @@ void lineynoe(int b, int c)
 
     double x = ((-1.0) * c) / b;
 
-    printf("Решением этого уравнения яляется: %f\n\n", x);
+    printf("Решением этого уравнения яляется: %.3f\n\n", x);
 
     printf("Введите три коэффицента квадратного уравнения a, b, c или q для завершения программы:");
 }
@@ -59,11 +79,10 @@ void qwadratnoe(int a, int b, int c)
     }
     else //два решения
     {
-        double x1 = ((-1.0) * b + sqrt(D)) / (2 * a);
-        double x2 = ((-1.0) * b - sqrt(D)) / (2 * a);
+        double x1 = ((-1.0) * b + sqrtmy((float)D)) / (2 * a);
+        double x2 = ((-1.0) * b - sqrtmy((float)D)) / (2 * a);
         printf(" и оно больше 0, это уравнение имеет два решения, равных: %.3f %.3f\n\n", x1, x2);
     }
 
     printf("Введите три коэффицента квадратного уравнения a, b, c или q для завершения программы:");
 }
-
