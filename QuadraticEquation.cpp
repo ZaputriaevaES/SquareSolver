2a1b2287c52df4f2726cc59651432781eddf8ddc
ZaputriaevaES <111437343+ZaputriaevaES@users.noreply.github.com>
2022-08-23 09:35:15 +0300

    Add files via upload

--- QuadraticEquation.cpp
+++ QuadraticEquation.cpp
@@ -2,12 +2,21 @@
 #include <TXLib.h>
 #include <math.h>
 
+enum NumberOfSolutions {ZERO = 0, ONE = 1, TWO = 2, INF = -1, MEANINGLESS = -2};
+enum EquationType {LINEAR = 1, QUADRATIC = 2};
+
+struct solutions {int type;
+                  int amount;
+                  double answer1;
+                  double answer2;};
+
 void InputAndOutputFormat(void); // выяснение формата ввода/вывода
+void InputCleaning(void); // очистка ввода
 void EnteringCoefficients(int input, int output); // получение коэффицентов уравнения
 void DerivationOfDecisions(int a, int b , int c, int outputformat); // вывод решений в нужном формате
-void SquareSolver(int a, int b, int c, double  * solutions); // выяснение типа уравнения
-void linearEquation(int b, int c, double  * solutions); // решение линейного уравнения
-void QuadraticEquation(int a, int b , int c, double  * solutions); // решение квадратного уравнения
+void SquareSolver(int a, int b, int c, struct solutions *); // выяснение типа уравнения
+void linearEquation(int b, int c, struct solutions *); // решение линейного уравнения
+void QuadraticEquation(int a, int b , int c, struct solutions *); // решение квадратного уравнения
 
 int main(void)
 {
@@ -17,24 +26,29 @@ int main(void)
 
 void InputAndOutputFormat(void)
 {
-    int input = 1, output = 1, ch;
+    int input = 1, output = 1;
     printf("Здравствуйте, эта программа решает квадратное уравнение вида: a * x^2 + b * x + c = 0.\n\n"
     "Если вы хотите вводить данные с клавиатуры, то введите 1, если считывать из файла, то 2: ");
     while (scanf("%d", &input) != 1)
     {
-        while ((ch = getchar()) != '\n')
-            putchar(ch);
+        InputCleaning();
         printf(" не является допустимым значением. Будте внимательнее, и введите 1 или 2: ");
     }
     printf("Отлично!\n"
            "Если вы хотите выводить данные на экран, то введите 1, если записывать в файл, то 2: ");
     while (scanf("%d", &output) != 1)
     {
-        while ((ch = getchar()) != '\n')
-            putchar(ch);
+        InputCleaning();
         printf(" не является допустимым значением. Будте внимательнее, и введите 1 или 2: ");
     }
     EnteringCoefficients(input, output);
+}
+
+void InputCleaning(void)
+{
+    int ch;
+    while ((ch = getchar()) != '\n')
+        putchar(ch);
 }
 
 void EnteringCoefficients(int input, int output)
@@ -89,58 +103,59 @@ void EnteringCoefficients(int input, int output)
 
 void DerivationOfDecisions(int a, int b , int c, int outputformat)
 {
-    double solutions[4] = {0};
-    SquareSolver(a, b, c, solutions);
-    if (outputformat == 1.0) // вывод на экран
+    struct solutions answers = {0, 0, 0, 0};
+    SquareSolver(a, b, c, &answers);
+    if (outputformat == 1) // вывод на экран
     {
-        if(solutions[0] == 1.0) // линейное
+        if(answers.type == LINEAR) // линейное
         {
-            if(solutions[1] == -1.0)
+            if(answers.amount == INF)
             {
                 printf("Уравнение вида: 0 * x^2 + 0 * x = 0 имеет бесконечное количество решений.\n\n");
             }
-            else if(solutions[1] == -2.0)
+            else if(answers.amount == MEANINGLESS)
             {
                 printf("Уравнение вида: %d = 0 не имеет смысла.\n\n", c);
             }
-            else if(solutions[1] == 1.0 && solutions[2] == 0.0)
+            else if(answers.amount == ONE && answers.answer1 == 0.0)
             {
                 printf("Уравнение вида: %d * x = 0 имеет единственное решение равное 0.\n\n", b);
             }
             else
             {
                 printf("Уравнение умеет вид: %d * x + %d = 0 и является линейным.\n"
-                       "Решением этого уравнения яляется: %.3f.\n\n", b, c, solutions[2]);
+                       "Решением этого уравнения яляется: %.3f.\n\n", b, c, answers.answer1);
             }
         }
         else // квадратное
         {
-            if(solutions[1] == 1.0 && solutions[2] == 0.0)
+            if(answers.amount == ONE && answers.answer1 == 0.0)
             {
                 printf("Уравнение вида: %d * x^2 = 0 имеет единственное решение равное 0.\n\n", a);
             }
-            else if(solutions[1] == 2.0 && solutions[2] == 0.0)
+            else if(answers.amount == TWO && answers.answer1 == 0.0)
             {
                 printf("Уравнение имеет вид: %d * x^2 + %d * x = 0\n"
-                    "и его решениями являются: 0, %.3f.\n\n", a, b, solutions[3]);
+                    "и его решениями являются: 0, %.3f.\n\n", a, b, answers.answer2);
             }
             else
             {
                 printf("Уравнение умеет вид: %d * x^2 + %d * x + %d = 0 и является квадратным.\n", a, b, c);
-                if (solutions[1] == 0.0)
+                if (answers.amount == ZERO)
                 {
                     printf("Т.к. значение дискриминанта меньше 0,\n"
                            "это уравнение не имеет решений.\n\n");
                 }
-                else if (solutions[1] == 1.0) //одно решение
+                else if (answers.amount == ONE) //одно решение
                 {
                     printf("Т.к. значение дискриминанта равно 0,\n"
-                           "это уравнение имеет одно решение, равное: %.3f.\n\n", solutions[2]);
+                           "это уравнение имеет одно решение, равное: %.3f.\n\n", answers.answer1);
                 }
                 else
                 {
                     printf("Т.к. значение дискриминанта больше 0,\n"
-                           "это уравнение имеет два решения, равных: %.3f, %.3f.\n\n", solutions[2], solutions[3]);
+                           "это уравнение имеет два решения, равных: %.3f, %.3f.\n\n",
+                           answers.answer1, answers.answer2);
                 }
             }
         }
@@ -149,78 +164,78 @@ void DerivationOfDecisions(int a, int b , int c, int outputformat)
     {
         FILE * write;
         write = fopen("write.txt", "a");
-        if(solutions[1] == -1.0 || solutions[1] == -2.0 || solutions[1] == 0.0)
+        if(answers.amount == INF || answers.amount == MEANINGLESS || answers.amount == ZERO)
         {
-            fprintf(write, "%.g\n", solutions[1]);
+            fprintf(write, "%d\n", answers.amount);
         }
-        else if(solutions[1] == 1.0)
+        else if(answers.amount == ONE)
         {
-             fprintf(write, "1 : %.3g\n", solutions[2]);
+             fprintf(write, "1 : %.3g\n", answers.answer1);
         }
         else
         {
-             fprintf(write, "2 : %.3g %.3g\n", solutions[2], solutions[3]);
+             fprintf(write, "2 : %.3g %.3g\n", answers.answer1, answers.answer2);
         }
     }
 }
 
-void SquareSolver(int a, int b, int c, double  * solutions)
+void SquareSolver(int a, int b, int c, struct solutions * answers)
 {
     if (a == 0)
     {
-        solutions[0] = 1.0; // является линейным
-        linearEquation(b, c, solutions);
+        answers->type = LINEAR; // является линейным
+        linearEquation(b, c, answers);
     }
 
     else // a != 0
     {
-        solutions[0] = 2.0; // является квадратным
-        QuadraticEquation(a, b, c, solutions);
+        answers->type = QUADRATIC; // является квадратным
+        QuadraticEquation(a, b, c, answers);
     }
 }
 
-void linearEquation(int b, int c, double  * solutions)
+void linearEquation(int b, int c, struct solutions * answers)
 {
     if (b == 0)
     {
         if (c == 0)
         {
-            solutions[1] = -1.0; // бесконечное количество решений
+            answers->amount = INF; // бесконечное количество решений
         }
         else // c != 0
         {
-            solutions[1] = -2.0; // не имеет смысла
+            answers->amount = MEANINGLESS; // не имеет смысла
         }
     }
     else // b != 0
     {
         if (c == 0)
         {
-            solutions[1] = 1.0;
-            solutions[2] = 0.0;
+            answers->amount = ONE;
+            answers->answer1 = 0.0;
         }
         else // c != 0
         {
-            solutions[1] = 1.0;
-            solutions[2] = (-1.0 * c) / b;
+            answers->amount = ONE;
+            answers->answer1 = (-1.0 * c) / b;
         }
     }
 }
 
-void QuadraticEquation(int a, int b, int c, double  * solutions)
+void QuadraticEquation(int a, int b, int c, struct solutions * answers)
 {
     if (c == 0)
     {
         if (b == 0)
         {
-            solutions[1] = 1.0;
-            solutions[2] = 0.0;
+            answers->amount = ONE;
+            answers->answer1= 0.0;
         }
         else // b != 0
         {
-            solutions[1] = 2.0;
-            solutions[2] = 0.0;
-            solutions[3] = (-1.0 * b) / a;
+            answers->amount = TWO;
+            answers->answer1 = 0.0;
+            answers->answer2 = (-1.0 * b) / a;
         }
     }
     else // c != 0
@@ -228,23 +243,19 @@ void QuadraticEquation(int a, int b, int c, double  * solutions)
         int D = b * b - 4 * a * c; //значение дискриминанта
         if (D < 0) //решений нет
         {
-            solutions[1] = 0.0;
+            answers->amount = ZERO;
         }
         else if (D == 0) //одно решение
         {
-            solutions[1] = 1.0;
-            solutions[2] = ((-1.0) * b) / (2 * a);
+            answers->amount = ONE;
+            answers->answer1 = ((-1.0) * b) / (2 * a);
         }
         else //два решения
         {
             double d = sqrt((double)D);
-            solutions[1] = 2.0;
-            solutions[2] = ((-1.0) * b + d) / (2 * a);
-            solutions[3] = ((-1.0) * b - d) / (2 * a);
+            answers->amount = TWO;
+            answers->answer1 = ((-1.0) * b + d) / (2 * a);
+            answers->answer2 = ((-1.0) * b - d) / (2 * a);
         }
     }
 }
-
-
-
-
